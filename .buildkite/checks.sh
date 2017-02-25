6e8018b69391167f8c6d18599891874e7d858f09
Andrey Astashov <mvc.aaa@gmail.com>
2017-02-25 14:54:40 +0300

    Changes

--- .buildkite/checks.sh
+++ .buildkite/checks.sh
@@ -1,6 +1,12 @@
 # Test bash script for deployment
 
-echo "Hello from develop branch"
+echo "Hello from $BUILDKITE_BRANCH branch"
+
+if [ -f "$BUILDKITE_AGENT_META_DATA_GIT_CACHE_DIR/tested-branches" ]; then 
+	sed -i.bak '/$BUILDKITE_BRANCH/d' "$BUILDKITE_AGENT_META_DATA_GIT_CACHE_DIR/tested-branches"
+elif
+	touch "$BUILDKITE_AGENT_META_DATA_GIT_CACHE_DIR/tested-branches"
+fi
 
 composer install 
 vendor/bin/phpunit --bootstrap src/Email.php tests/EmailTest
