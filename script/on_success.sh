b3c6cc54f1fb371b80200d5bc6d09ab3f6797d68
Andrey Astashov <mvc.aaa@gmail.com>
2017-02-25 13:56:16 +0300

    Changes

--- script/on_success.sh
+++ script/on_success.sh
@@ -7,8 +7,5 @@ echo $BUILDKITE_BUILD_CHECKOUT_PATH
 # current branch name
 echo $BUILDKITE_BRANCH
 
-echo $BUILDKITE_AGENT_NAME 
-
-baseDir=$(grep -Po "(?<=git_cache_dir=).*" | curl "https://api.buildkite.com/v2/organizations/equals-self-equals/agents?name=$BUILDKITE_AGENT_NAME&access_token=e993fe6a2b043933b4e6cd4a18e47382cead1689")
-
-echo $baseDir
+# Agent meta
+echo $BUILDKITE_AGENT_META_DATA_GIT_CACHE_DIR
