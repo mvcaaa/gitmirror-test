4fd9351fd8070c511e56a7ee546b986ed3ee785b
Andrey Astashov <mvc.aaa@gmail.com>
2017-02-25 17:16:04 +0300

    remove unneded file

--- .buildkite/checks.sh
+++ .buildkite/checks.sh
@@ -3,7 +3,7 @@
 echo "Hello from $BUILDKITE_BRANCH branch"
 
 if [ -f "$BUILDKITE_AGENT_META_DATA_GIT_CACHE_DIR/tested-branches" ]; then 
-	sed -i.bak 's/$BUILDKITE_BRANCH//g;/^$/d' $BUILDKITE_AGENT_META_DATA_GIT_CACHE_DIR/tested-branches
+	sed -i.bak "s/$BUILDKITE_BRANCH//g;/^$/d" $BUILDKITE_AGENT_META_DATA_GIT_CACHE_DIR/tested-branches
 else
 	touch $BUILDKITE_AGENT_META_DATA_GIT_CACHE_DIR/tested-branches
 fi
