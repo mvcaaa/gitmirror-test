b6226b38d4c2354d924a4dd54ef8a8b863950a6e
Andrey Astashov <mvc.aaa@gmail.com>
2017-02-25 13:18:05 +0300

    Clean deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,9 +5,3 @@ echo "Hello from develop branch"
 composer install 
 vendor/bin/phpunit --bootstrap src/Email.php tests/EmailTest
 
-#current folder
-echo $BUILDKITE_BUILD_CHECKOUT_PATH
-# current branch name
-echo $BUILDKITE_BRANCH
-
-buildkite-agent meta-data get "git_cache_dir"
--- script/on_success.sh
+++ script/on_success.sh
@@ -1,3 +1,10 @@
 # Test bash script for deployment
 
 echo "Hello from on success script"
+
+#current folder
+echo $BUILDKITE_BUILD_CHECKOUT_PATH
+# current branch name
+echo $BUILDKITE_BRANCH
+
+buildkite-agent meta-data get "git_cache_dir"
