82c8b767654a8b7162d38f439cf216c34329a54a
Andrey Astashov <mvc.aaa@gmail.com>
2017-02-25 13:42:34 +0300

    Changes

--- script/on_success.sh
+++ script/on_success.sh
@@ -7,4 +7,6 @@ echo $BUILDKITE_BUILD_CHECKOUT_PATH
 # current branch name
 echo $BUILDKITE_BRANCH
 
-buildkite-agent meta-data get "queue"
+echo $BUILDKITE_AGENT_NAME 
+
+curl "https://api.buildkite.com/v2/organizations/equals-self-equals/agent/$BUILDKITE_AGENT_NAME?access_token=e993fe6a2b043933b4e6cd4a18e47382cead1689"
